b98800debd1f5f0ed3b014a3e8b3709be2a3a07b
Nikhil Malviya <nikhilmalviya1662000@gmail.com>
2019-09-13 23:46:12 +0530

    comments removed

--- src/main/java/com/project/WelcomeController.java
+++ src/main/java/com/project/WelcomeController.java
@@ -29,20 +29,6 @@ public class WelcomeController {
 
     @RequestMapping("/")
     public String index(Model model) {
-//        User newUser = new User("helloworld11", "helloworld", UserType.PROFESSOR, UserStatus.ACTIVE);
-//        Student student = new Student("Nikhil Malviya", null, "99999", "Helloworld");
-//        student.setUser(newUser);
-//        studentRepository.save(student);
-//        this.userRepository.save(newUser);
-//        studentRepository.deleteById(1);
-//        Course course = new Course("Java Master Class");
-//        course.setStudents(new ArrayList<>());
-//        course.getStudents().add(new Student("Nikhil", null, "9999999999", "helloworld"));
-//        course.getStudents().add(new Student("Nikhil Malviya", null, "9999999999", "helloworld"));
-//        course.getStudents().add(new Student("nikmalviya", null, "9999999999", "helloworld"));
-//        courseRepository.save(course);
-
-//        studentRepository.deleteById(3);
         Course course = courseRepository.findByCourseTitleLike("Spring%");
         Student student = studentRepository.findById(5).orElse(null);
         assert student != null;
