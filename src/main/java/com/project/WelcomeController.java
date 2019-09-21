2cabf6152bd0143fa9d75b9d548bf42d85347299
mistrymanan <manan.cba1718@ict.gnu.ac.in>
2019-09-21 12:44:33 +0530

    all-hibernate-mapping-done

--- src/main/java/com/project/WelcomeController.java
+++ src/main/java/com/project/WelcomeController.java
@@ -1,8 +1,8 @@
 package com.project;
 
-import com.project.repository.CourseRepository;
-import com.project.repository.StudentRepository;
-import com.project.repository.UserRepository;
+import com.project.entity.*;
+import com.project.repository.*;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,13 +15,20 @@ public class WelcomeController {
     private UserRepository userRepository;
     private StudentRepository studentRepository;
     private CourseRepository courseRepository;
+    private ProfessorRepository professorRepository;
+    private SubjectRepository subjectRepository;
 
     public WelcomeController(UserRepository userRepository,
+                             ProfessorRepository professorRepository,
                              StudentRepository studentRepository,
-                             CourseRepository courseRepository) {
+                             CourseRepository courseRepository,
+                             SubjectRepository subjectRepository
+                             ) {
         this.userRepository = userRepository;
         this.studentRepository = studentRepository;
         this.courseRepository = courseRepository;
+        this.professorRepository = professorRepository;
+        this.subjectRepository=subjectRepository;
     }
 //
 //    public WelcomeController(StudentRepository studentRepository) {
@@ -36,7 +43,18 @@ public class WelcomeController {
 //        assert student != null;
 //        student.getCourses().add(course);
 //        studentRepository.save(student);
-        model.addAttribute("students", this.studentRepository.findAll());
-        return "index";
+//        model.addAttribute("students", this.studentRepository.findAll());
+//        Course course = new Course("CBA");
+//        User user = new User("mananmistry", "Sherlock", UserType.PROFESSOR, UserStatus.ACTIVE);
+//        Professor professor = new Professor("Manan Mistry", null, "7096693099", "new", user);
+//        courseRepository.save(course);
+//        professorRepository.save(professor);
+        //Test Case.
+//        Course course=courseRepository.findById(1).orElse(null);
+//        Professor professor=professorRepository.findById(1).orElse(null);
+//        Subject subject=new Subject("DBMS",course,professor);
+//        subjectRepository.save(subject);
+//        course.getSubjects().forEach(System.out::println);
+       return "index";
     }
 }
--- src/main/java/com/project/entity/Submission.java
+++ src/main/java/com/project/entity/Submission.java
@@ -9,21 +9,29 @@ public class Submission {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "submission_id")
     private int submissionID;
-    //Remaining
+    @ManyToOne(cascade = {
+            CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH
+    })
+    @JoinColumn(name = "assignment_id")
     private Assignment assignment;
-    //Remaining
-    @ManyToOne
+    @ManyToOne(cascade = {
+            CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH
+    })
+    @JoinColumn(name = "subject_id")
     private Subject subject;
-    @ManyToOne
+    @ManyToOne(cascade = {
+            CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH
+    })
+    @JoinColumn(name = "student_id")
     private Student student;
     @Column(name = "status")
     private String status;
-    @Column(name = "makrs")
+    @Column(name = "marks")
     private Float marks;
     @Column(name = "file_path")
     private String filePath;
 
-    public Submission(){
+    public Submission() {
     }
 
     public Submission(Assignment assignment, Subject subject, Student student, String status, Float marks, String filePath) {
