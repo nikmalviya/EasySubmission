092c3d6507eaf49df546fd78a702ea2b4abaa6d9
mistrymanan <manan.cba1718@ict.gnu.ac.in>
2019-11-26 12:44:12 +0530

    Storage-Service Bug Solved

--- src/main/java/com/project/service/AssignmentService.java
+++ src/main/java/com/project/service/AssignmentService.java
@@ -7,21 +7,19 @@ import com.project.repository.AssignmentRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
-import java.util.Date;
 import java.util.List;
 
 @Service
 public class AssignmentService {
     private AssignmentRepository assignmentRepository;
-
-    public AssignmentService(AssignmentRepository assignmentRepository) {
+    final private StorageService storageService;
+    public AssignmentService(AssignmentRepository assignmentRepository, StorageService storageService) {
         this.assignmentRepository = assignmentRepository;
+        this.storageService = storageService;
     }
 
     public List<Assignment> getAssignmentList() {
@@ -42,32 +40,24 @@ public class AssignmentService {
 
     public void saveAssignment(AssignmentForm assignmentForm, Subject subject) throws IOException {
         MultipartFile multiPartFile = assignmentForm.getFile();
-        String url = "src/main/resources/uploads/assignments/file$$" + multiPartFile.getOriginalFilename() + "$$-" + subject.getSubjectID() + "-" + new Date().toLocaleString();
-        File file = new File(url);
-        FileOutputStream fos = new FileOutputStream(file);
-        byte[] bytes = multiPartFile.getBytes();
-        fos.write(bytes);
-        fos.close();
-        assignmentForm.setFilePath(url);
+        String fileUrl=this.storageService.uploadAssignment(multiPartFile,assignmentForm.getAssignmentTitle(),subject.getSubjectID(),subject.getProfessor().getProfessorID());
+        System.out.println(fileUrl);
+        assignmentForm.setFilePath(fileUrl);
         assignmentForm.setSubject(subject);
         Assignment assignment = new Assignment(assignmentForm);
-        System.out.println("Save Assignment from service");
         this.saveAssignment(assignment);
-
-
+//        String url = "src/main/resources/uploads/assignments/file$$" + multiPartFile.getOriginalFilename() + "$$-" + subject.getSubjectID() + "-" + new Date().toLocaleString();
+//        File file = new File(url);
+//        FileOutputStream fos = new FileOutputStream(file);
+//        byte[] bytes = multiPartFile.getBytes();
+//        fos.write(bytes);
+//        fos.close();
     }
 
     public void updateAssignment(Assignment assignment, AssignmentForm assignmentForm, Subject subject) throws IOException {
-
         MultipartFile multiPartFile = assignmentForm.getFile();
-        String url = "src/main/resources/uploads/assignments/file$$" + multiPartFile.getOriginalFilename() + "$$-" + subject.getSubjectID() + "-" + new Date().toString();
-//        String url="/home/mananmistry/Projects/SEM5/Final/OnlineSubmissionSystem/src/main/webapp/WEB-INF/uploads/"+multiPartFile.getOriginalFilename();
-        File file = new File(url);
-        FileOutputStream fos = new FileOutputStream(file);
-        byte[] bytes = multiPartFile.getBytes();
-        fos.write(bytes);
-        fos.close();
-        assignmentForm.setFilePath(url);
+        String fileUrl=this.storageService.uploadAssignment(multiPartFile,assignmentForm.getAssignmentTitle(),subject.getSubjectID(),subject.getProfessor().getProfessorID());
+        assignmentForm.setFilePath(fileUrl);
         assignmentForm.setSubject(subject);
         assignment.setAssignmentTitle(assignmentForm.getAssignmentTitle());
         assignment.setDeadlineDate(assignmentForm.getDeadlinedate());
